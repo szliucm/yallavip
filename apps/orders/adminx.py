e1c0e205a91be7235fc5a350374bc1ff7b236fa8
szliucm <szliucm@126.com>
2018-12-10 08:36:23 +0300

    11.26 shopify品类设置

--- apps/orders/adminx.py
+++ apps/orders/adminx.py
@@ -333,7 +333,7 @@ class OrderAdmin(object):
                             "specialservice": [
                                 {
                                     "servicename": "W7",
-                                    "costamount": row.order_amount,
+                                    "costamount": str(int(row.order_amount)),
                                     "costcurrency": "SAR",
                                     "description": ""
                                 }
