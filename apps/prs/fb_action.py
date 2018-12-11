a07461660c77b0430046c134636ac89fe1f8d6cb
szliucm <szliucm@126.com>
2018-12-11 12:22:56 +0300

    11.26 shopify品类设置

--- apps/prs/fb_action.py
+++ apps/prs/fb_action.py
@@ -170,7 +170,7 @@ def post_creative_feed():
             feed_post_id = feed_post.get_id()
 
             if feed_post_id:
-                MyProductFb.objects.filter(pk=fb.pk).update(fb_id=feed_post_id, published=True,published_time=datetime.utctime() )
+                MyProductFb.objects.filter(pk=fb.pk).update(fb_id=feed_post_id, published=True,published_time=datetime.utcnow() )
 
                 print("Wow ", page_id, feed_post_id, feed_post.link)
 
