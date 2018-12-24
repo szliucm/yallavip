015ef9a94b62cbc5201dd2b0439b073f3cf82a10
szliucm <szliucm@126.com>
2018-12-25 02:23:01 +0300

    11.26 shopify品类设置

--- apps/prs/tasks.py
+++ apps/prs/tasks.py
@@ -403,7 +403,7 @@ def get_aliproduct(pk, offer_id,cate_code):
     from .ali import get_ali_product_info
     import time
 
-    time.sleep(random.uniform(60,120))
+    time.sleep(random.uniform(1,30))
     message, status = get_ali_product_info(offer_id, cate_code)
     if status is False:
         AliProduct.objects.filter(pk=pk).update(created_error=message)
