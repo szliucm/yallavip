120cc747e2ac826e29cf050fc254e07c49711b14
szliucm <szliucm@126.com>
2019-01-27 00:30:30 +0300

    11.26 shopify品类设置

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -87,12 +87,12 @@ class Order(models.Model):
     warhouse_comment = models.CharField(u'拣货备注', max_length=500, default='', null=True, blank=True)
     cs_comment = models.CharField(u'客服备注', max_length=500, default='', null=True, blank=True)
 
-    receiver_name = models.CharField(u'收货人姓名', default='', max_length=500, blank=True)
-    receiver_addr1 = models.CharField(u'地址1', default='', max_length=500, blank=True)
+    receiver_name = models.CharField(u'收货人姓名', default='', max_length=500, null=True,blank=True)
+    receiver_addr1 = models.CharField(u'地址1', default='', max_length=500, null=True,blank=True)
     receiver_addr2 = models.CharField(u'地址2', default='', max_length=500, null=True,blank=True)
-    receiver_city = models.CharField(u'收货人城市', default='', max_length=500, blank=True)
-    receiver_country = models.CharField(u'收货人国家', default='', max_length=500, blank=True)
-    receiver_phone = models.CharField(u'收货人电话', default='', max_length=500, blank=True)
+    receiver_city = models.CharField(u'收货人城市', default='', max_length=500, null=True,blank=True)
+    receiver_country = models.CharField(u'收货人国家', default='', max_length=500, null=True,blank=True)
+    receiver_phone = models.CharField(u'收货人电话', default='', max_length=500, null=True,blank=True)
 
     package_no = models.CharField(u'包裹号', default='', max_length=100, blank=True)
     logistic_no = models.CharField(u'物流追踪号', default='', max_length=100, blank=True)
