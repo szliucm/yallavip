fcde6cff1225b6c3f367f4b4d2f9c2ab1180e9dc
szliucm <szliucm@126.com>
2019-01-26 03:18:45 +0300

    11.26 shopify品类设置

--- apps/shop/tasks.py
+++ apps/shop/tasks.py
@@ -441,7 +441,8 @@ def get_orders():
 
     url = shop_url + "/admin/orders/count.json"
     params = {
-        "since_id": max_shoporiorder_no
+        "since_id": max_shoporiorder_no,
+        "status ": "any",
     }
     # print("url %s params %s"%(url, params))
     r = requests.get(url, params)
