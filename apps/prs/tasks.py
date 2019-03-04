bd340fc0df218d0bb3a547e560b550a07b21bd2a
szliucm <szliucm@126.com>
2019-03-04 15:35:37 +0300

    11.26 shopify品类设置

--- apps/prs/tasks.py
+++ apps/prs/tasks.py
@@ -1642,15 +1642,11 @@ def delete_outstock_lightin_album():
                 photo_list.append(lightinalbum.fb_id)
 
             lightinalbums_out[lightinalbum.myalbum.page_no] = photo_list
-            print("lightinalbum is %s  page no is %s, photo_id is %s ",lightinalbum, lightinalbum.myalbum.page_no,lightinalbum.fb_id )
+            print("lightinalbum is %s  page no is %s, photo_id is %s "%(lightinalbum, lightinalbum.myalbum.page_no,lightinalbum.fb_id ))
 
     # 删除子集
-    #debug
-    #delete_out_lightin_album(lightinalbums_out)
 
-
-    #更新spu的发布记录
-    #Lightin_SPU.objects.filter(Q(quantity__isnull=True)|Q(quantity=0),published=True).update(published=False)
+    delete_out_lightin_album(lightinalbums_out)
 
 #删除lightin_album 的某个特定子集
 def delete_out_lightin_album(lightinalbums_out):
