2baf8c9b1224a20a3f96946c416d9f52dad3e51c
szliucm <szliucm@126.com>
2019-03-05 10:57:51 +0300

    11.26 shopify品类设置

--- apps/prs/tasks.py
+++ apps/prs/tasks.py
@@ -1949,7 +1949,7 @@ def sync_Shipped_order_lightin():
 def sync_Shipped_order_shopify():
     from prs.shop_action import  fulfill_order_shopify
 
-    orders = Order.objects.filter(status = "open", wms_status= "D",logistic_no__isnull=False, fulfillment_status__isnull=True )
+    orders = Order.objects.filter(status = "open", wms_status= "D",logistic_no__isnull=False, fulfillment_status__isnull=True ,order_id__isnull=False)
 
     for order in orders:
 
