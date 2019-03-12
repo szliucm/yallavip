ca7bd383be586e9a749d9fa263d5c03f343114a3
szliucm <szliucm@126.com>
2019-03-12 05:23:16 +0300

    11.26 shopify品类设置

--- apps/logistic/models.py
+++ apps/logistic/models.py
@@ -480,11 +480,11 @@ class Resell(Package):
         return self.logistic_no
 
 class LogisticTrail(models.Model):
-    waybillnumber = models.CharField(u'物流追踪号', default='', max_length=100, blank=True)
-    trail_status  = models.CharField(u'发生状态', default='', max_length=500, blank=True)
-    trail_statuscnname = models.CharField(u'发生状态_zh', default='', max_length=500, blank=True)
+    waybillnumber = models.CharField(u'物流追踪号', default='', max_length=100, blank=True, null=True)
+    trail_status  = models.CharField(u'发生状态', default='', max_length=500, blank=True, null=True)
+    trail_statuscnname = models.CharField(u'发生状态_zh', default='', max_length=500, blank=True, null=True)
     trail_time = models.DateTimeField(u'发生时间', auto_now=False, blank=True, null=True)
-    trail_locaiton= models.CharField(u'发生地点', default='', max_length=100, blank=True)
+    trail_locaiton= models.CharField(u'发生地点', default='', max_length=100, blank=True, null=True)
 
 
     class Meta:
