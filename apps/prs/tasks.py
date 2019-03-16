4e01af147faa153c46d02155478c26859c2b9f71
szliucm <szliucm@126.com>
2019-03-16 11:38:43 +0300

    11.26 shopify品类设置

--- apps/prs/tasks.py
+++ apps/prs/tasks.py
@@ -3018,7 +3018,7 @@ def combo_image(combo):
         draw1 = ImageDraw.Draw(layer)
         # 简单打货号
         lw, lh = layer.size
-        draw1.rectangle(( 20,lh -130, 20+len(combo.SKU)*30, lh-80) , fill='yellow')
+        draw1.rectangle(( 20,lh -130, 20+len(combo.SKU)*30, lh-60) , fill='yellow')
         draw1.text(( 30,lh -120), combo.SKU, font=font,
                    fill=(0, 0, 0))  # 设置文字位置/内容/颜色/字体
 
