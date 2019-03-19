92e066b91e928e697cf0805d68b4fb5285bd2eb1
changminliu <aaron@honhot.com>
2019-03-19 21:35:57 +0800

    传相册

--- apps/prs/tasks.py
+++ apps/prs/tasks.py
@@ -1654,7 +1654,7 @@ def sync_lightin_album():
 
     batch_nos = lightinalbums_all.values_list('myalbum').annotate(Min('batch_no'))
     for batch_no in batch_nos:
-        lightinalbums = lightinalbums_all.filter(myalbum__pk = batch_no[0],batch_no=batch_no[1],lightin_spu__sellable__gt=0)
+        lightinalbums = lightinalbums_all.filter(Q(lightin_spu__sellable__gt=0) | Q(lightin_sku__o_sellable__gt=0),myalbum__pk = batch_no[0],batch_no=batch_no[1],)
 
         for lightinalbum in lightinalbums:
             error, posted = post_lightin_album(lightinalbum)
