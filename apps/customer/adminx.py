7a835c34ef5e49b6ea83d6cebe3b42c27eb86a31
szliucm <szliucm@126.com>
2019-04-01 14:42:46 +0300

    03.19

--- apps/customer/adminx.py
+++ apps/customer/adminx.py
@@ -622,7 +622,7 @@ class DraftAdmin(object):
         print("aaaaaaaaaaaaaaa",obj.pk, obj.lightin_sku.lightin_spu.handle, obj.lightin_sku.SKU)
         order_details = OrderDetail.objects.filter(order__status ="open", order__customer = obj.customer,sku = obj.lightin_sku.SKU )
         if order_details:
-            order_quantity = order_details[0].quantity
+            order_quantity = order_details[0].product_quantity
         else:
             order_quantity = 0
         print("BBBBBBB", obj.lightin_sku.o_sellable + int(order_quantity))
