3fa4cc701b35fb0650b7aa630b181066ed187b3f
szliucm <szliucm@126.com>
2019-04-01 17:49:49 +0300

    03.19

--- apps/customer/adminx.py
+++ apps/customer/adminx.py
@@ -654,9 +654,12 @@ class DraftAdmin(object):
         obj = self.new_obj
 
         obj.save()
+        return
 
-
-
+    def do_post(self, request):
+        context = super().do_post(self, request)
+        print("xxxxxxxxxxxxxx")
+        return
 
 
 
