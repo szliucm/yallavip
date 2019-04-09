f6b7fcb69960e7f11a238d35c5693d4d850a7c84
szliucm <szliucm@126.com>
2019-04-09 03:56:58 +0300

    03.19

--- apps/prs/adminx.py
+++ apps/prs/adminx.py
@@ -856,8 +856,8 @@ class LightinAlbumAdmin(object):
     list_display = ["lightin_spu","sellable",  "myalbum","page","batch_no","name","photo", "fb_id", "published","publish_error","published_time",]
     # 'sku_name','img',
 
-    search_fields = ["lightin_spu__SPU",]
-    list_filter = ["myalbum", "published","myalbum__mypage","material","batch_no","deleted", ]
+    search_fields = ["lightin_spu__SPU","myalbum",]
+    list_filter = [ "published","myalbum__mypage","material","batch_no","deleted", ]
     list_editable = []
     readonly_fields = ()
     actions = []
