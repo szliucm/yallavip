6cc789e579171691f023f5c4d57f7af0dd58d197
szliucm <szliucm@126.com>
2019-04-09 06:14:40 +0300

    03.19

--- apps/commodity/adminx.py
+++ apps/commodity/adminx.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
+__author__ = 'Aaron'
 
-# Register your models here.
+import  json
+import xadmin
+from .models import *
+
+@xadmin.sites.register(SelectionRule)
+class SelectionRuleAdmin(object):
+
+    actions = [ ]
+    list_display = ('name', 'cates','prices', 'attrs',)
+    list_editable = ['name', 'cates','prices', 'attrs',]
+    search_fields = ['name', 'cates','prices', 'attrs', ]
+    list_filter = ()
+
+    exclude = []
+    ordering = []
--- apps/fb/models.py
+++ apps/fb/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 
-#from shop.models import  ShopifyProduct,ShopifyVariant
-
-
-
-
+from commodity.models import  SelectionRule
 
 # Create your models here.
 class MyPage(models.Model):
@@ -162,65 +158,7 @@ class MyFeed(models.Model):
         #return 'business.facebook.com'+ self.link
         return  self.feed_no
 
-#选品规则
-class SelectionRule(models.Model):
-    name = models.CharField(u'选品规则', default='', max_length=200, blank=True)
 
-    cates = models.CharField(max_length=300,null=True, blank=True, verbose_name="关联品类",help_text="多个品类用逗号隔开")
-    prices = models.CharField(max_length=300, null=True, blank=True, verbose_name="价格区间",help_text="最低价最高价，用逗号隔开")
-    attrs = models.CharField(max_length=300, null=True, blank=True, verbose_name="规格",help_text="多个规格用逗号隔开")
-
-    class Meta:
-        verbose_name = "选品规则"
-        verbose_name_plural = verbose_name
-
-
-    def __str__(self):
-        return self.name
-
-#套装规则
-class ComboRule(models.Model):
-    selection_rule = models.ForeignKey(SelectionRule, related_name='selection_combo', null=True, blank=True,
-                                     verbose_name="选品规则", on_delete=models.CASCADE)
-
-    name = models.CharField(u'套装规则', default='', max_length=200, blank=True)
-    items_count = models.CharField(max_length=300, null=True, blank=True, verbose_name="数量区间",help_text="用逗号隔开")
-    items_amount = models.CharField(max_length=300, null=True, blank=True, verbose_name="金额区间", help_text="用逗号隔开")
-
-    class Meta:
-        verbose_name = "套装规则"
-        verbose_name_plural = verbose_name
-
-
-    def __str__(self):
-        return self.name
-
-
-#调价规则
-class PriceRule(models.Model):
-    selection_rule = models.ForeignKey(SelectionRule, related_name='selection_price', null=True, blank=True,
-                                     verbose_name="选品规则", on_delete=models.CASCADE)
-
-    name = models.CharField(u'调价规则', default='', max_length=200, blank=True)
-    TYPE = (
-        ("取整","取整"),
-        ("倍数", "倍数"),
-    )
-
-
-
-    type = models.CharField(u'类型', choices=TYPE,default='', max_length=200, blank=True)
-    rules = models.CharField(max_length=300, null=True, blank=True, verbose_name="定价规则")
-
-    commit = models.BooleanField(u'是否提交', default=False)
-
-    class Meta:
-        verbose_name = "调价规则"
-        verbose_name_plural = verbose_name
-
-
-    def __str__(self):
-        return self.name
 
 class MyAlbum(models.Model):
     selection_rule = models.ForeignKey(SelectionRule, related_name='selection_album', null=True, blank=True,
