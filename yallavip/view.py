f7a09a1175d5db8de78d3c9db4009c639d56aff9
szliucm <szliucm@126.com>
2019-04-10 11:00:58 +0300

    03.19

--- yallavip/view.py
+++ yallavip/view.py
@@ -1,7 +1,11 @@
-
+from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    context          = {}
-    context['hello'] = 'Hello World!'
-    return render(request, 'hello.html', context)
+    if request.method == "GET":
+        name = request.GET.get('domain_name')
+        print name
+        result = "OK!"
+        return HttpResponse(result)
+
+
