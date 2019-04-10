b5bc844c3ed1bedf8e83717a1baaa31b6bd22202
szliucm <szliucm@126.com>
2019-04-10 11:07:22 +0300

    03.19

--- yallavip/view.py
+++ yallavip/view.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def hello(request):
     if request.method == "GET":
         response = request.GET.get('response')
-        print(response)
+        print("从前台返回的结果",response)
         result = "OK!"
         return HttpResponse(result)
 
