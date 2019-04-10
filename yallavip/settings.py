2927781908a92c9dacf86a6ca97b2c2e7e767006
szliucm <szliucm@126.com>
2019-04-10 03:59:54 +0300

    03.19

--- yallavip/settings.py
+++ yallavip/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djangosecure',
     #'rules',
     'users',      #注册app
 
@@ -83,6 +84,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'yallavip.urls'
