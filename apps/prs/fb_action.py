f0c37aef56c4c449901570278110643aadb4174e
szliucm <szliucm@126.com>
2019-04-12 13:54:12 +0300

    03.19

--- apps/prs/fb_action.py
+++ apps/prs/fb_action.py
@@ -1301,7 +1301,7 @@ def post_yallavip_ad():
     # 取库存大、单价高、已经发布到相册 且还未打广告的商品
 
 
-    lightinalbums_all = LightinAlbum.objects.filter(lightin_spu__sellable__gt=5,lightin_spu__SPU__istartswith = "s",
+    lightinalbums_all = LightinAlbum.objects.filter(lightin_spu__sellable__gt=0,lightin_spu__SPU__istartswith = "s",
                                                 lightin_spu__shopify_price__gt=30, #lightin_spu__shopify_price__lt=50,
                                                 aded=False,
                                                 yallavip_album__page__active=True,yallavip_album__page__is_published=True,
@@ -1424,7 +1424,8 @@ def combo_ad_image(spu_ims, spus_name):
             return None
         ims.append(im)
 
-    # 开始拼图
+    # 开始拼图exit
+
 
     item_count = spu_ims.count()
     print("图片数量", item_count)
