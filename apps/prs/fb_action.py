2778dc832a1a354327cd2a7772cfce5d26fd4e2d
szliucm <szliucm@126.com>
2019-04-12 12:41:21 +0300

    03.19

--- apps/prs/fb_action.py
+++ apps/prs/fb_action.py
@@ -1,4 +1,4 @@
-from .models import MyProductShopify,MyProductFb,Token, LightinAlbum,Lightin_SPU
+from .models import MyProductShopify,MyProductFb,Token, LightinAlbum,Lightin_SPU,YallavipAd
 from fb.models import  MyPage,MyAlbum,MyPhoto
 from shop.models import ShopifyProduct, ShopifyVariant,ShopifyOptions
 from shop.models import  ShopifyImage
