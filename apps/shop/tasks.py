b05495bd8ac6f66a9ede2a9da7eae1c610ab18cf
szliucm <szliucm@126.com>
2019-04-17 10:18:22 +0300

    03.19

--- apps/shop/tasks.py
+++ apps/shop/tasks.py
@@ -826,7 +826,7 @@ def download_product():
                 "limit": limit,
                 #"since_id": max_product_no,
                 #"created_at_min": "2019-02-10T00:00:00-00:00",
-                "fields": "id,handle,body_html,title,product_type,created_at,published_at,"
+                "fields": "id,handle,body_html,title,product_type,created_at,published_at,published_scope,"
                           "updated_at,tags,vendor,variants,images,options",
                 # "fields": "product_id",
             }
@@ -841,7 +841,7 @@ def download_product():
 
 
         except Exception as e:
-            print(e)
+            print("出错了！", e)
             print("products for the shop {} completed".format(shop_name))
             break
 
