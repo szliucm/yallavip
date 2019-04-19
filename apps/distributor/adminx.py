d62c44c400ba77bc3610ca4088404afab8814943
szliucm <szliucm@126.com>
2019-04-19 05:51:34 +0300

    03.19

--- apps/distributor/adminx.py
+++ apps/distributor/adminx.py
@@ -1,7 +1,41 @@
 import xadmin
 from django.utils.safestring import mark_safe
 
-from .models import Yallavip_SKU
+from .models import Yallavip_SPU, Yallavip_SKU
+
+@xadmin.sites.register(Yallavip_SPU)
+class Yallavip_SPUAdmin(object):
+
+    def photo(self, obj):
+        if obj.images is not None and len(obj.images)>0 :
+            photos = json.loads(obj.images)
+            img = ''
+
+            for photo in photos:
+                try:
+                    img = img + '<a><img src="%s" width="100px"></a>' % (photo)
+                except Exception as e:
+                    print("获取图片出错", e)
+
+            return mark_safe(img)
+
+        else:
+            photos = "no photo"
+
+    photo.short_description = "图片"
+
+    list_display = [ "SPU","handle", "sellable", "en_name","cn_name", "cate_1","cate_2","cate_3", ]
+    # 'sku_name','img',
+
+    search_fields = ["SPU","handle", ]
+    list_filter = ["cate_1","cate_2","cate_3","got","got_time","got_error","vendor"]
+    list_editable = []
+    readonly_fields = ()
+    actions = []
+
+    def queryset(self):
+        qs = super().queryset()
+        return qs.filter( vendor = "gw")
 
 @xadmin.sites.register(Yallavip_SKU)
 class Yallavip_SKUAdmin(object):
@@ -35,3 +69,7 @@ class Yallavip_SKUAdmin(object):
     list_editable = []
     readonly_fields = ()
     actions = []
+
+    def queryset(self):
+        qs = super().queryset()
+        return qs.filter( lightin_spu__vendor = "gw")
--- apps/distributor/models.py
+++ apps/distributor/models.py
@@ -1,7 +1,16 @@
 from django.db import models
-from prs.models import Lightin_SKU
+from prs.models import Lightin_SPU, Lightin_SKU
 
 # Create your models here.
+class Yallavip_SPU(Lightin_SPU):
+    class Meta:
+        proxy = True
+
+        verbose_name = "SPU"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.SPU
 
 class Yallavip_SKU(Lightin_SKU):
     class Meta:
@@ -11,4 +20,4 @@ class Yallavip_SKU(Lightin_SKU):
         verbose_name_plural = verbose_name
 
     def __str__(self):
-        return self.page_no
+        return self.SKU
