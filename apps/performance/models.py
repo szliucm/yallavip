5fd928c6be086a47a24efdd66ae8da7a0a89c714
szliucm <szliucm@126.com>
2019-04-20 03:54:04 +0300

    03.19

---
+++ apps/performance/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Sales(models.Model):
+    order_date = models.DateField(u'订单日期', auto_now=False, null=True, blank=True)
+    type = models.CharField(u'类型', choices=TYPE, default='', max_length=200, blank=True)
+    count = models.IntegerField(u'数量', default=0, blank=True, null=True)
+
+
+    class Meta:
+        verbose_name = "销售业绩"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.order_date
