0981d94e9fa3a8d183ae4199f2fd109a469f6bd4
szliucm <szliucm@126.com>
2019-04-20 04:03:08 +0300

    03.19

--- apps/performance/adminx.py
+++ apps/performance/adminx.py
@@ -2,6 +2,35 @@ import xadmin
 
 from .models import *
 
+
+
+def update_performance(days=None):
+    from django.db.models import Count, Sum, Q
+    import pytz
+    import datetime
+    # from django.utils import timezone as dt
+    from django.db.models.functions import TruncDate
+
+    today = datetime.date.today()
+
+    riyadh = pytz.timezone('Asia/Riyadh')
+    orders = Order.objects.all()
+    if days:
+        orders = orders.filter(order_time__range=(today - datetime.timedelta(days=days), today))
+
+    order_counts = orders.annotate(date=TruncDate("order_time", tzinfo=riyadh)) \
+        .values("date", "status").annotate(orders=Count("order_no")).order_by("-date")
+
+    sales = {}
+    for order_count in order_counts:
+        obj, created = Sales.objects.update_or_create(order_date=order_count.date,
+                                                      type=order_count.status,
+                                                      defaults={'count': order_count.orders,
+
+                                                                }
+                                                      )
+
+
 @xadmin.sites.register(Sales)
 class SalesAdmin(object):
     list_display = ["order_date", "type", 'count',  ]
@@ -11,36 +40,13 @@ class SalesAdmin(object):
     list_filter = [ ]
     list_editable = []
     readonly_fields = ()
-    actions = ["update_performance", ]
+    actions = ["batch_update_performance", ]
     ordering = ['-order_date']
 
     #更新最近7天的销售记录
-    def update_performance(self, request, queryset):
-        from django.db.models import Count, Sum, Q
-        import pytz
-        import datetime
-        #from django.utils import timezone as dt
-        from django.db.models.functions import  TruncDate
+    def batch_update_performance(self, request, queryset):
 
-        today = datetime.date.today()
+        update_performance(7)
 
-        riyadh = pytz.timezone('Asia/Riyadh')
-
-        order_counts = Order.objects.filter(order_time__range=(today - datetime.timedelta(days=7), today))\
-            .annotate(date=TruncDate("order_time", tzinfo=riyadh))\
-            .values("date", "status").annotate(orders=Count("order_no")).order_by("-date")
-
-        sales = {}
-        for order_count in order_counts:
-
-            obj, created = Sales.objects.update_or_create(order_date=order_count.date,
-                                                          type = order_count.status,
-                                                           defaults={'count': order_count.orders,
-
-                                                                     }
-                                                           )
-
-
-
-    update_performance.short_description = "更新销售业绩"
+    batch_update_performance.short_description = "更新销售业绩"
 
