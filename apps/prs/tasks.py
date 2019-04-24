96e4ecd9274307770623db4f7779847bf413ef2d
szliucm <szliucm@126.com>
2019-04-24 14:08:41 +0300

    03.19

--- apps/prs/tasks.py
+++ apps/prs/tasks.py
@@ -4560,9 +4560,11 @@ def prepare_yallavip_album_material(page_no=None):
         print(lightinalbums)
 
         for lightinalbum in lightinalbums:
+            prepare_a_album.apply_async((lightinalbum.pk), queue='fb')
+
+            '''
             spu = lightinalbum.lightin_spu
             sku = lightinalbum.lightin_sku
-
 
             if sku:
                 LightinAlbum.objects.filter(pk=lightinalbum.pk).update(
@@ -4637,6 +4639,89 @@ def prepare_yallavip_album_material(page_no=None):
                     LightinAlbum.objects.filter(pk=lightinalbum.pk).update(
                         material_error=error
                     )
+            '''
+
+def prepare_a_album(lightinalbum_pk):
+    lightinalbum = LightinAlbum.objects.get(pk=lightinalbum_pk)
+
+    spu = lightinalbum.lightin_spu
+    sku = lightinalbum.lightin_sku
+
+    if sku:
+        LightinAlbum.objects.filter(pk=lightinalbum.pk).update(
+            image_marked=sku.image_marked,
+
+            # batch_no=batch_no,
+            material=True
+        )
+
+    elif spu:
+        error = ""
+        # 准备文字
+        # 标题
+        title = spu.title
+        # 货号
+        if title.find(spu.handle) == -1:
+            name = title + "  [" + spu.handle + "]"
+        else:
+            name = title
+        # 规格
+        lightin_skus = Lightin_SKU.objects.filter(SPU=spu.SPU)
+        options = []
+        for sku in lightin_skus:
+            option = sku.skuattr
+            if option not in options:
+                options.append(option)
+
+        if len(options) > 0:
+            name = name + "\n\nSkus:  "
+
+        for option in options:
+            name = name + "\n\n   " + option
+
+        # 价格
+        price1 = int(spu.yallavip_price)
+        price2 = int(price1 * random.uniform(5, 6))
+        # 为了减少促销的麻烦，文案里不写价格了
+        # name = name + "\n\nPrice:  " + str(price1) + "SAR"
+
+        # 准备图片
+        # 先取第一张，以后考虑根据实际有库存的sku的图片（待优化）
+        if spu.images_dict:
+            image = json.loads(spu.images_dict).values()
+            if image and len(image) > 0:
+                a = "/"
+                image_split = list(image)[0].split(a)
+
+                image_split[4] = '800x800'
+                image = a.join(image_split)
+
+            # 打水印
+            # logo， page促销标
+            # 如果有相册促销标，就打相册促销标，否则打价格标签
+
+            image_marked, image_marked_url = yallavip_mark_image(image, spu.handle, str(price1), str(price2),
+                                                                 lightinalbum)
+            if not image_marked:
+                error = "打水印失败"
+
+        else:
+            print(album, spu.SPU, "没有图片")
+            error = "没有图片"
+
+        if error == "":
+            LightinAlbum.objects.filter(pk=lightinalbum.pk).update(
+                name=name,
+                image_marked=image_marked_url,
+                # batch_no=batch_no,
+                material=True
+            )
+        else:
+            LightinAlbum.objects.filter(pk=lightinalbum.pk).update(
+                material_error=error
+            )
+
+
 
 def cal_price():
     #spus = Lightin_SPU.objects.filter(sellable__gt=0)
