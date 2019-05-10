72ae67514cd28b21c2b228cf594fb1293380c6ac
szliucm <szliucm@126.com>
2019-05-10 09:43:35 +0300

    03.19

--- apps/prs/adminx.py
+++ apps/prs/adminx.py
@@ -1195,9 +1195,9 @@ class PagePromoteCateAdmin(object):
     list_editable = []
     search_fields = ['mypage__page', 'cate__name','promote_cate__name' ]
     list_filter = ('cate',)
-    #filter_horizontal = ('cate','promote_cate')
+    filter_horizontal = ('cate',)
     style_fields = {'cate': 'm2m_transfer',
-                    'promote_cate': 'm2m_transfer'
+
                     }
 
     exclude = []
