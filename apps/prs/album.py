fce4c701f2bd3affebc7a12a26cbd06ba571b88e
szliucm <szliucm@126.com>
2019-05-10 11:45:57 +0300

    03.19

--- apps/prs/album.py
+++ apps/prs/album.py
@@ -1,9 +1,61 @@
-@shared_task
+from prs.fb_action import  get_token
+from facebook_business.api import FacebookAdsApi
+from facebook_business.adobjects.page import Page
+
+from fb.models import MyPage, MyAlbum
+from prs.models. import YallavipAlbum,PagePromoteCate
+
+
 #根据page cate 规则，更新page的相册
 #将page中失效的相册找出来并删掉
 #未创建的则创建之
+
+def create_album(page_no , album_name ):
+    access_token, long_token = get_token(page_no)
+    FacebookAdsApi.init(access_token=access_token)
+
+    fields = ["created_time", "description", "id",
+              "name", "count", "updated_time", "link",
+              "likes.summary(true)", "comments.summary(true)"
+              ]
+    params = {
+            'name': album_name,
+            'location': 'Riyadh Region, Saudi Arabia',
+            #'privacy': 'everyone',
+            #'place': '111953658894021',
+            'message':"Yallavip's most fashion flash sale "+ album_name,
+
+                }
+    album = Page(page_no).create_album(
+                            fields=fields,
+                            params=params,
+                        )
+    #插入到待返回的相册列表中
+    if album:
+
+        #保存到数据库中
+        obj, created = MyAlbum.objects.update_or_create(album_no=album["id"],
+                                                    defaults={'page_no': page_no,
+                                                              'created_time': album["created_time"],
+                                                              'updated_time': album["updated_time"],
+
+                                                              'name': album["name"],
+                                                              'count': album["count"],
+                                                              'like_count': album["likes"]["summary"][
+                                                                  "total_count"],
+                                                              'comment_count': album["comments"]["summary"][
+                                                                  "total_count"],
+                                                              'link': album["link"],
+
+                                                              }
+                                                    )
+
+
+
+
+    return  obj
+
 def sync_cate_album(page_no=None):
-    from django.db import connection, transaction
 
     # 找出所有活跃的page
     pages = MyPage.objects.filter(active=True)
@@ -29,13 +81,11 @@ def sync_cate_album(page_no=None):
                 yallvip_album.filter(catesize__in=cate_sizes).update(active=True)
 
                 #相册里没有的，要创建
-                catesize_to_add = cate_sizes.exclude(id__in = yallvip_album.values("catesize__pk")).distinct()
+
+                catesize_to_add = cate_sizes.exclude(id__in = yallvip_album.filter(catesize__isnull=False).values_list("catesize__pk",flat=True)).distinct()
                 # 根据规则创建相册，成功后记录到数据库里
 
                 for catesize in catesize_to_add:
-                    print(catesize.cate.name+ " "+ catesize.size)
-                    continue
-
                     new_album = create_album(page.page_no, catesize.cate.name+ " "+ catesize.size)
                     YallavipAlbum.objects.create(
                         page=page,
