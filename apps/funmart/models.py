4251f127c6a18c7994854fd02e9638c68ebad58e
szliucm <szliucm@126.com>
2019-05-28 11:09:50 +0300

    03.19

--- apps/funmart/models.py
+++ apps/funmart/models.py
@@ -6,12 +6,12 @@ class FunmartOrder(models.Model):
     order_no = models.CharField(u'order_no', default='', max_length=50, blank=True)
     track_code = models.CharField(u'track_code', default='', max_length=50, blank=True)
     ship_method = models.CharField(u'ship_method', default='', max_length=50, blank=True)
-    upload_date = models.DateField(u'扫描日期', auto_now=True, null=True, blank=True)
-    downloaded =  models.BooleanField(u"已下载", default=False)
-    dealed = models.BooleanField(u"已处理", default=False)
+    upload_date = models.DateField(u'upload_date', auto_now=True, null=True, blank=True)
+    downloaded =  models.BooleanField(u"downloaded", default=False)
+    dealed = models.BooleanField(u"dealed", default=False)
 
     class Meta:
-        verbose_name = "Funmart 订单"
+        verbose_name = "FunmartOrder"
         verbose_name_plural = verbose_name
 
 
@@ -24,13 +24,13 @@ class FunmartOrderItem(models.Model):
     order_no = models.CharField(u'order_no', default='', max_length=50, blank=True)
     sku = models.CharField(u'sku', default='', max_length=50, blank=True)
 
-    quantity = models.IntegerField(u'数量', default=0, blank=True, null=True)
+    quantity = models.IntegerField(u'quantity', default=0, blank=True, null=True)
     price = models.CharField(u'Price', default='', max_length=50, blank=True)
 
 
 
     class Meta:
-        verbose_name = "Funmart 订单明细"
+        verbose_name = "FunmartOrderItem"
         verbose_name_plural = verbose_name
 
 
@@ -46,12 +46,12 @@ class FunmartSPU(models.Model):
 
     en_name = models.CharField(default='', max_length=300, null=True, blank=True, verbose_name="en_name")
 
-    skuattr = models.TextField(default='', null=True, blank=True, verbose_name="属性字典")
-    description = models.TextField(default='', null=True, blank=True, verbose_name="描述")
-    images = models.TextField(default='', null=True, blank=True, verbose_name="图片")
+    skuattr = models.TextField(default='', null=True, blank=True, verbose_name="skuattr")
+    description = models.TextField(default='', null=True, blank=True, verbose_name="description")
+    images = models.TextField(default='', null=True, blank=True, verbose_name="images")
     link = models.CharField(default='', max_length=300, null=True, blank=True, verbose_name="link")
-    sale_price = models.FloatField(verbose_name="供方实际销售价", default=0)
-    skuList = models.TextField(default='', null=True, blank=True, verbose_name="图片")
+    sale_price = models.FloatField(verbose_name="sale_price", default=0)
+    skuList = models.TextField(default='', null=True, blank=True, verbose_name="skuList")
 
     class Meta:
         verbose_name = "Funmart SPU"
@@ -61,15 +61,15 @@ class FunmartSPU(models.Model):
         return self.SPU
 
 class FunmartSKU(models.Model):
-    funmart_spu = models.ForeignKey(FunmartSPU, null=True, blank=True, verbose_name="SPU外键",
+    funmart_spu = models.ForeignKey(FunmartSPU, null=True, blank=True, verbose_name="funmart_spu",
                                     related_name="funmartspu_sku", on_delete=models.CASCADE)
     SPU = models.CharField(default='',max_length=300, null=True, blank=True, verbose_name="SPU")
     SKU = models.CharField(default='', max_length=100, null=True, blank=True, verbose_name="SKU")
 
-    skuattr = models.TextField(default='', null=True, blank=True, verbose_name="属性字典")
+    skuattr = models.TextField(default='', null=True, blank=True, verbose_name="skuattr")
 
-    images = models.TextField(default='', null=True, blank=True, verbose_name="sku图")
-    sale_price = models.FloatField(verbose_name="供方实际销售价", default=0)
+    images = models.TextField(default='', null=True, blank=True, verbose_name="images")
+    sale_price = models.FloatField(verbose_name="sale_price", default=0)
 
     class Meta:
         verbose_name = "Funmart SKU"
