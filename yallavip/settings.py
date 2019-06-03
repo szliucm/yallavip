3ae9eca7d7a6944926511ef895d7f69fae99859a
szliucm <szliucm@126.com>
2019-06-03 08:53:38 +0300

    03.19

--- yallavip/settings.py
+++ yallavip/settings.py
@@ -170,7 +170,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'collectedstatic/')
 DATA_UPLOAD_MAX_NUMBER_FIELDS = None
