4e3e61f23c4594ee2cbc8c419d41763971349f64
szliucm <szliucm@126.com>
2019-06-04 07:07:06 +0300

    03.19

--- apps/funmart/views.py
+++ apps/funmart/views.py
@@ -66,8 +66,23 @@ def scanpackage(request):
         elif not (track_code and order_ref):
                 item['scan_result'] = 'Please Input code'
         else:
+            #从funmart查，并更新本地数据库
+            #暂时没有考虑效率问题
+
             order, orderitem_list = get_funmart_order(track_code=track_code, order_ref=order_ref,batch_no=batch_no)
             if order:
+                #更新批次扫描记录
+
+                scan_order, created = ScanOrder.objects.update_or_create(
+                    track_code=order.track_code,
+                    order_no=order.order_no,
+                    batch_no= batch_no,
+                    defaults={
+                        'downloaded': True,
+                        'shelfed': False
+                    }
+                )
+
                 item['track_code'] =  order.track_code
                 item['order_no'] = order.order_no
                 item['order_ref'] = order.order_ref
@@ -81,88 +96,88 @@ def scanpackage(request):
         return JsonResponse(item)
 
 def scanpackageitem(request):
-    from funmart.tasks import get_funmart_sku
-    if request.method == 'GET':
-        pass
-    elif request.method == 'POST':
-        item = {}
-        posts = request.POST
-        print(posts)
-        track_code = posts.get('track_code')
-        item_code = posts.get('item_code')
-        if track_code == "":
-            item['package'] = 'Please Input track_code'
-            return JsonResponse(item)
+from funmart.tasks import get_funmart_sku
+if request.method == 'GET':
+pass
+elif request.method == 'POST':
+item = {}
+posts = request.POST
+print(posts)
+track_code = posts.get('track_code')
+item_code = posts.get('item_code')
+if track_code == "":
+item['package'] = 'Please Input track_code'
+return JsonResponse(item)
 
 
-        #查询包裹信息
-        scanorders = ScanOrder.objects.filter(track_code = track_code)
-        scanorder_items = ScanOrderItem.objects.filter(track_code=track_code)
-        if not (scanorders and scanorder_items):
-            order, orderitem_list = get_funmart_order(track_code, order_no, batch_no)
-        else:
-            order = scanorders[0]
-            orderitem_list = scanorder_items
+#查询包裹信息
+scanorders = ScanOrder.objects.filter(track_code = track_code)
+scanorder_items = ScanOrderItem.objects.filter(track_code=track_code)
+if not (scanorders and scanorder_items):
+order, orderitem_list = get_funmart_order(track_code, order_no, batch_no)
+else:
+order = scanorders[0]
+orderitem_list = scanorder_items
 
-        if item_code:
-            try:
-                sku = FunmartBarcode.objects.get(barcode=item_code).SKU
+if item_code:
+try:
+    sku = FunmartBarcode.objects.get(barcode=item_code).SKU
 
-            except:
-                sku = get_funmart_barcode(item_code)
-                if not sku:
-                    item['item_code'] = 'No SKU '
-                    return item
+except:
+    sku = get_funmart_barcode(item_code)
+    if not sku:
+        item['item_code'] = 'No SKU '
+        return item
 
-            try:
-                scanorder_item = scanorder_items.get(sku=sku)
-                scanorder_item.scanned_quantity =F("scanned_quantity") +1
+try:
+    scanorder_item = scanorder_items.get(sku=sku)
+    scanorder_item.scanned_quantity =F("scanned_quantity") +1
 
-            except:
-                item['item_code'] = 'No SKU '
-                return  item
+except:
+    item['item_code'] = 'No SKU '
+    return  item
 
-        scanorder_items.refresh_from_db()
+scanorder_items.refresh_from_db()
 
-        scanorder_items.values_list("sku","name","quantity","scanned_quantity","action")
+scanorder_items.values_list("sku","name","quantity","scanned_quantity","action")
 
-        item()
+item()
 
-        item['package'] = "OK"
-        item['scanorder_items'] = scanorder_items
+item['package'] = "OK"
+item['scanorder_items'] = scanorder_items
 
-        return JsonResponse(item)
+return JsonResponse(item)
 
 
 def scanitem(request):
-    from funmart.tasks import get_funmart_barcode
-    if request.method == 'GET':
-        pass
-    elif request.method == 'POST':
-        item = {}
-        posts = request.POST
-        print(posts)
+from funmart.tasks import get_funmart_barcode
+if request.method == 'GET':
+pass
+elif request.method == 'POST':
+item = {}
+posts = request.POST
+print(posts)
 
-        item_code = posts.get('item_code').replace("－","-")
+item_code = posts.get('item_code').replace("－","-")
 
 
-        funmartbarcodes = FunmartBarcode.objects.filter(barcode=item_code)
-        if not funmartbarcodes:
-            funmart_sku = get_funmart_barcode(item_code)
-            print("get from funmart", funmart_sku)
-        else:
-            funmart_sku = funmartbarcodes[0].funmart_sku
+funmartbarcodes = FunmartBarcode.objects.filter(barcode=item_code)
+if not funmartbarcodes:
+funmart_sku = get_funmart_barcode(item_code)
+print("get from funmart", funmart_sku)
+else:
+funmart_sku = funmartbarcodes[0].funmart_sku
 
-            print("get from yallavip", funmart_sku)
+print("get from yallavip", funmart_sku)
 
-        if funmart_sku:
-            SKU = str(funmart_sku.id).zfill(9)
-            item["sku"] = SKU[:5] + '-' + SKU[5:]
-            item["sku_name"] = funmart_sku.name
-        else:
-            item["sku"] = "Not Found"
+if funmart_sku:
+SKU = str(funmart_sku.id).zfill(9)
+item["sku"] = SKU[:5] + '-' + SKU[5:]
+item["sku_name"] = funmart_sku.name
+else:
+item["sku"] = "Not Found"
 
-        return JsonResponse(item)
+return JsonResponse(item)
 
 
 
