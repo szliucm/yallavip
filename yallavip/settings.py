a60425e9fd6b13993f1e51ab16bca54f6e2c857e
szliucm <szliucm@126.com>
2019-06-09 12:17:29 +0300

    03.19

--- yallavip/settings.py
+++ yallavip/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import  sys
+import djqgrid
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0,os.path.join(BASE_DIR,'apps'))      #将应用包加入系统变量，便于模块导入
