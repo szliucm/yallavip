67aeadded17bd3510132c2469421ae17b2c678e0
szliucm <szliucm@126.com>
2019-06-17 09:23:29 +0300

    03.19

--- apps/fb/models.py
+++ apps/fb/models.py
@@ -398,8 +398,8 @@ class ImageTemplate(models.Model):
     size = models.CharField(verbose_name='尺寸',choices=SIZES,  default='', max_length=100,  null=False,blank=False)
 
     promote_template = models.ImageField(verbose_name='促销模版', upload_to='promote_template/', default="",  null=False,blank=False)
-    main_image_count = models.IntegerField(  null=False,blank=False verbose_name="主图数量")
-    sub_image_count = models.IntegerField( null=False,blank=False verbose_name="辅图数量")
+    main_image_count = models.IntegerField(  null=False,blank=False ,verbose_name="主图数量")
+    sub_image_count = models.IntegerField( null=False,blank=False, verbose_name="辅图数量")
 
     update_time = models.DateTimeField(verbose_name='更新时间', auto_now=True, null=True, blank=True)
 
