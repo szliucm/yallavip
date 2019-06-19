f32381706d6489ed823b7458ef62609a40d4e4cb
szliucm <szliucm@126.com>
2019-06-19 09:01:40 +0300

    03.19

--- apps/prs/tasks.py
+++ apps/prs/tasks.py
@@ -39,12 +39,12 @@ ad_tokens = "EAAHZCz2P7ZAuQBANXASqgDJV7vsZCKn4KoZCNAyEFUzWV4XFlvs3exbtdQJrLrVzku
 DEBUG = False
 
 if DEBUG:
-    warehouse_code = "TW02"
+    WAREHOUSE_CODE = "TW02"
     shipping_method = "L002-KSA-TEST",
     appToken = "85413bb8f6a270e1ff4558af80f2bef5"
     appKey = "9dca0be4c02bed9e37c1c4189bc1f41b"
 else:
-    warehouse_code = "W07"
+    WAREHOUSE_CODE = "W07"
     shipping_methods = ["ARAMEX_KSA","FETCHR_SAUDI_DOM"]
     #shipping_method = "FETCHR_SAUDI_DOM"
     #shipping_method = "ARAMEX_KSA"
@@ -2259,7 +2259,7 @@ def fulfill_order_lightin(order):
     param = {
         "platform": "B2C",
         "allocated_auto": "1",
-        "warehouse_code": warehouse_code,
+        "warehouse_code": WAREHOUSE_CODE,
         "shipping_method": shipping_method,
         "reference_no": order.order_no,
         # "order_desc":"\u8ba2\u5355\u63cf\u8ff0",
@@ -2579,20 +2579,25 @@ def sync_wms_quantity():
         print(mysql)
         my_custom_sql(mysql)
 
-def get_wms_quantity(barcodes=[]):
+def get_wms_quantity(barcodes=[], warehouse_code=None):
+    if not warehouse_code:
+        warehouse_code = WAREHOUSE_CODE
+
+
     page = 1
     pages, result = get_wms_quantity_page(page, barcodes)
 
     print("一共 %s页" % pages)
+
     while page < pages and result:
 
         print("正在处理第 %s 页" % page)
-        get_wms_quantity_page.apply_async((page, barcodes), queue='wms')
+        get_wms_quantity_page.apply_async((page, barcodes,warehouse_code), queue='wms')
 
         page += 1
 
 @shared_task
-def get_wms_quantity_page(page, barcodes):
+def get_wms_quantity_page(page, barcodes, warehouse_code):
 
     param = {
         "pageSize": "100",
@@ -2645,7 +2650,7 @@ def get_wms_quantity_page(page, barcodes):
 
 def getShippingMethod():
     param = {
-        "warehouseCode": warehouse_code,
+        "warehouseCode": WAREHOUSE_CODE,
     }
 
     service = "getShippingMethod"
