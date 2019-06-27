9364b93a46faf8ee6e6e8b9c6e7902f8ddd31b9f
szliucm <szliucm@126.com>
2019-06-27 04:48:32 +0300

    03.19

--- apps/prs/adminx.py
+++ apps/prs/adminx.py
@@ -249,7 +249,7 @@ class Lightin_barcodeResource(resources.ModelResource):
         skip_unchanged = True
         report_skipped = True
         import_id_fields = ('barcode',)
-        fields = ( "SKU","barcode","y_sellable")
+        fields = ( "SKU","barcode","y_sellable","warehouse_code")
         # exclude = ()
 
 @xadmin.sites.register(Lightin_barcode)
