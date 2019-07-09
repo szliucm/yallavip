5ede64f8010897f435981a4fc7a45226e54c99b0
szliucm <szliucm@126.com>
2019-07-09 14:36:38 +0300

    03.19

--- apps/prs/album.py
+++ apps/prs/album.py
@@ -355,7 +355,7 @@ def prepare_yallavip_album_material(page_no=None):
 
     for album in albums_list:
         #lightinalbums = lightinalbums_all.filter(yallavip_album=album).order_by("lightin_spu__sellable")[:100]
-        lightinalbums = lightinalbums_all.filter(yallavip_album=album).order_by("lightin_spu__sellable")[:100]
+        lightinalbums = lightinalbums_all.filter(yallavip_album=album).order_by("?")[:100]
         print(lightinalbums)
 
         for lightinalbum in lightinalbums:
